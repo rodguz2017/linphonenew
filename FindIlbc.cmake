0922507165f92556cb6f127683da88bbcef23c83
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-03-10 16:53:15 +0100

    Support build with CMake.

---
+++ FindIlbc.cmake
@@ -0,0 +1,54 @@
+############################################################################
+# FindIlbc.cmake
+# Copyright (C) 2014  Belledonne Communications, Grenoble France
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+############################################################################
+#
+# - Find the libilbc include file and library
+#
+#  ILBC_FOUND - system has libilbc
+#  ILBC_INCLUDE_DIRS - the libilbc include directory
+#  ILBC_LIBRARIES - The libraries needed to use libilbc
+
+set(_ILBC_ROOT_PATHS
+	${CMAKE_INSTALL_PREFIX}
+)
+
+find_path(ILBC_INCLUDE_DIRS
+	NAMES ilbc/iLBC_encode.h
+	HINTS _ILBC_ROOT_PATHS
+	PATH_SUFFIXES include
+)
+if(ILBC_INCLUDE_DIRS)
+	set(HAVE_ILBC_ILBC_ENCODE_H 1)
+endif()
+
+find_library(ILBC_LIBRARIES
+	NAMES ilbc
+	HINTS _ILBC_ROOT_PATHS
+	PATH_SUFFIXES bin lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Ilbc
+	DEFAULT_MSG
+	ILBC_INCLUDE_DIRS ILBC_LIBRARIES HAVE_ILBC_ILBC_ENCODE_H
+)
+
+mark_as_advanced(ILBC_INCLUDE_DIRS ILBC_LIBRARIES HAVE_ILBC_ILBC_ENCODE_H)
